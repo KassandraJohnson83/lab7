c7a41f999f56096255ab31d54647dc5851a21c01
KassandraJohnson83 <59705381+KassandraJohnson83@users.noreply.github.com>
2020-02-20 12:21:51 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var projects = require('../projects.json');
 
-/*
- * GET home page.
- */
 
 exports.view = function(request, response){
+	projects["viewAlt"]=false
   	response.render('index', projects);
 };
+
+exports.view = dunction(req,res){
+	projects["viewAlt"]=true;
+	res.render('index', projects);
+}
